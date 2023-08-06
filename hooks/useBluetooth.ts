c6875eb67ce72ab7faeadd6df59d84bf1b2d977a
milk717 <robolindasoo@gmail.com>
2023-08-06 18:58:32 +0900

    feat: 장치 검색 완료

--- hooks/useBluetooth.ts
+++ hooks/useBluetooth.ts
@@ -5,9 +5,13 @@ import {useState} from 'react';
 import showToast from '../components/Toast';
 
 export default function useBluetooth() {
+  //UI 관련
+  const [discovering, setDiscovering] = useState(false);
+
   const [pairedDeviceList, setPairedDeviceList] = useState<BluetoothDevice[]>(
     [],
   );
+  const [scanDeviceList, setScanDeviceList] = useState<BluetoothDevice[]>([]);
 
   const getPairedDevices = async () => {
     try {
@@ -15,16 +19,51 @@ export default function useBluetooth() {
       setPairedDeviceList(paired);
     } catch (err: any) {
       showToast({
-        autoHide: false,
         type: 'error',
         message: '페어링된 디바이스 가져오기 실패',
         description: err?.message,
+        autoHide: true,
       });
+    }
+  };
+
+  const getScanDevices = async () => {
+    try {
+      setDiscovering(true);
+
+      showToast({
+        type: 'info',
+        message: '장치를 검색중입니다. 잠시만 기다려주세요.',
+        description: undefined,
+        autoHide: false,
+      });
+
+      const unpaired = await RNBluetoothClassic.startDiscovery();
+      const namedDeviceList = unpaired.filter(v => v.name !== v.address);
+      setScanDeviceList(namedDeviceList);
+
+      showToast({
+        type: 'success',
+        message: `${namedDeviceList.length}개의 장치를 발견했습니다.`,
+        description: undefined,
+        autoHide: true,
+      });
+    } catch (err: any) {
+      showToast({
+        type: 'error',
+        message: '장치 검색 실패',
+        description: err.message,
+        autoHide: true,
+      });
+    } finally {
+      setDiscovering(false);
     }
   };
 
   return {
     pairedDeviceList,
     getPairedDevices,
+    scanDeviceList,
+    getScanDevices,
   };
 }
--- screens/BluetoothScreen.tsx
+++ screens/BluetoothScreen.tsx
@@ -5,6 +5,7 @@ import React, {useEffect} from 'react';
 import BluetoothList from '../components/BluetoothList';
 import usePermission from '../hooks/usePermission';
 import useBluetooth from '../hooks/useBluetooth';
+import {Button} from '@rneui/themed';
 
 interface Props {
   navigation: BluetoothScreenNavigationProp;
@@ -12,7 +13,8 @@ interface Props {
 
 const BluetoothScreen: React.FC<Props> = ({navigation}) => {
   const {requestPermissions} = usePermission();
-  const {pairedDeviceList, getPairedDevices} = useBluetooth();
+  const {pairedDeviceList, getPairedDevices, scanDeviceList, getScanDevices} =
+    useBluetooth();
 
   useEffect(() => {
     requestPermissions(isGranted => {
@@ -35,10 +37,11 @@ const BluetoothScreen: React.FC<Props> = ({navigation}) => {
         />
         <BluetoothList
           title={'검색된 디바이스'}
-          data={pairedDeviceList}
+          data={scanDeviceList}
           onPress={address => handleBluetoothPress(address)}
         />
       </View>
+      <Button onPress={getScanDevices}>장치 검색 시작</Button>
     </SafeAreaView>
   );
 };
