691cda402bfed014ac1cdce080c94506eb6796ea
Sumin <robolindasoo@gmail.com>
2024-03-26 08:41:17 +0900

    fix: 라이브러리 타입 에러 임시로 해결

--- src/screens/WifiScreen.tsx
+++ src/screens/WifiScreen.tsx
@@ -2,20 +2,26 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {FlatList, Text} from 'react-native';
 import useWifiList from '../hooks/useWifiList';
 import ListItem from '../components/common/ListItem';
+import {cssInterop} from 'nativewind';
 
 const WifiScreen = () => {
   const {wifiList} = useWifiList();
 
+  // @ts-expect-error
+  const StyledSafeAreaView = cssInterop(SafeAreaView, {className: 'style'});
+
   return (
-    <SafeAreaView>
+    <StyledSafeAreaView className="bg-white h-screen">
       <Text>WIFI</Text>
       <FlatList
         data={wifiList}
+        className="px-4"
+        contentContainerClassName="flex gap-y-1"
         renderItem={({item: {SSID, BSSID}}) => (
           <ListItem title={SSID} subTitle={BSSID} />
         )}
       />
-    </SafeAreaView>
+    </StyledSafeAreaView>
   );
 };
 
