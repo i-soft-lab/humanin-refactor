4061e6b9eb468022a1f36b7b9bd27e46acfb5f19
Sumin <robolindasoo@gmail.com>
2024-03-25 14:18:58 +0900

    chore: tailwind 사용하도록 세팅 완료

--- App.tsx
+++ App.tsx
@@ -1,27 +1,24 @@
-import React, {useState, useEffect, createContext} from 'react';
-import GraphScreen from './screens/GraphScreen';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 import {RootStackParamList} from './types/navigationType';
-import BleScreen from './screens/BleScreen';
-import {BleProvider} from './context/BleProvider';
 import BottomTabs from './types/bottomTabs';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { LanguageProvider } from './context/LanguageProvider';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {LanguageProvider} from './context/LanguageProvider';
+import './global.css';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App: React.FC = () => {
-
   return (
     <LanguageProvider>
-      <SafeAreaProvider>      
+      <SafeAreaProvider>
         <GestureHandlerRootView style={{flex: 1}}>
           <NavigationContainer>
-            <BottomTabs/>
-            <Toast/>
+            <BottomTabs />
+            <Toast />
           </NavigationContainer>
         </GestureHandlerRootView>
       </SafeAreaProvider>
