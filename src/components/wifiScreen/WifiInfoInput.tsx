28ad2c45fa4b54fbe7474dfdcb4821aa03df13f9
Sumin <robolindasoo@gmail.com>
2024-03-26 17:05:36 +0900

    refactor: 와이파이 정보 입력하는 로직 다이얼로그 밖으로 뺌

--- src/components/wifiScreen/WifiInfoInput.tsx
+++ src/components/wifiScreen/WifiInfoInput.tsx
@@ -1,19 +1,23 @@
-import {useState} from 'react';
-import {Dialog} from '@rneui/themed';
-import {Text, TextInput, TouchableHighlight} from 'react-native';
+import {
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import {Icon} from '@rneui/base';
 
-type WifiInfoDialogProps = {
-  visible: boolean;
+type WifiInfoInputProps = {
   ssid?: string;
-  closeDialog: () => void;
-  onClose: (form: FormData) => void;
+  onCancel: () => void;
+  onSubmit: (form: FormData) => void;
 };
 
-const WifiInfoDialog: React.FC<WifiInfoDialogProps> = ({
-  visible,
+const WifiInfoInput: React.FC<WifiInfoInputProps> = ({
   ssid,
-  closeDialog,
-  onClose,
+  onCancel,
+  onSubmit,
 }) => {
   const [isNotValid, setIsNotValid] = useState(false);
   const [form, setForm] = useState({
@@ -47,15 +51,17 @@ const WifiInfoDialog: React.FC<WifiInfoDialogProps> = ({
     }
 
     const formData = getFormData();
-    closeDialog();
-    onClose(formData);
+    onSubmit(formData);
   };
 
   return (
-    <Dialog isVisible={visible} onBackdropPress={closeDialog}>
-      <Text className="mb-4 font-psemibold text-gray-800">
-        연결에 필요한 정보를 입력하세요
-      </Text>
+    <View className="px-6">
+      <View className="flex flex-row gap-4 items-center mb-8">
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="arrow-back-ios-new" type="material" />
+        </TouchableOpacity>
+        <Text className="font-pnormal text-xl text-black">{ssid}</Text>
+      </View>
       <Text
         className={`mb-1 font-pnormal text-sm ${
           isNotValid ? 'text-red-400' : ''
@@ -66,6 +72,8 @@ const WifiInfoDialog: React.FC<WifiInfoDialogProps> = ({
         className={`border rounded px-2 mb-2 font-pnormal ${
           isNotValid ? 'border-red-400' : 'border-gray-300'
         }`}
+        placeholder="password"
+        placeholderTextColor="#bbb"
         onChangeText={e => handleFormChange(e, 'passwd')}
         value={form.passwd}
       />
@@ -85,17 +93,17 @@ const WifiInfoDialog: React.FC<WifiInfoDialogProps> = ({
         value={form.topic}
       />
       {isNotValid && (
-        <Text className="mb-2 font-plight text-xs text-right text-red-500">
+        <Text className="mb-2 font-plight text-xs text-red-500">
           올바른 정보를 입력하세요
         </Text>
       )}
       <TouchableHighlight
-        className="bg-blue-950 py-2.5 rounded"
+        className="bg-blue-950 py-2.5 rounded w-20 self-end"
         onPress={handleSubmitButtonPress}>
         <Text className="text-white text-center font-psemibold">전송</Text>
       </TouchableHighlight>
-    </Dialog>
+    </View>
   );
 };
 
-export default WifiInfoDialog;
+export default WifiInfoInput;
--- src/screens/WifiScreen.tsx
+++ src/screens/WifiScreen.tsx
@@ -2,39 +2,34 @@ import {FlatList, Text, View} from 'react-native';
 import useWifiList from '../hooks/useWifiList';
 import ListItem from '../components/common/ListItem';
 import React, {useState} from 'react';
-import WifiInfoDialog from '../components/wifiScreen/WifiInfoDialog';
 import ScreenLayout from '../components/common/ScreenLayout';
 import {SendForm} from './SettingsScreen';
 import axios from 'axios';
 import {showErrorToast, showSuccessToast} from '../components/Toast';
 import ResetReceiver from '../components/wifiScreen/ResetReceiver';
+import WifiInfoInput from '../components/wifiScreen/WifiInfoInput';
 
 const WifiScreen = () => {
   const {wifiList} = useWifiList();
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [selectedSSID, setSelectedSSID] = useState<string | undefined>();
 
   const handleSubmitForm = async (formData: FormData) => {
-    setIsDialogOpen(false);
     setIsLoading(true);
     const response = await axios
       .post<SendForm>('http://192.168.4.1/api/ssid', formData)
+      .then(_ => showSuccessToast('와이파이 설정 완료'))
       .catch(_ => {
         setIsError(false);
         setSelectedSSID(undefined);
         showErrorToast('와이파이 설정 실패');
       });
-    showSuccessToast('와이파이 설정 완료');
     console.log(response);
     setIsLoading(false);
   };
 
-  const toggleDialogOpen = () => setIsDialogOpen(prev => !prev);
-
   const handleSSIDPress = (value: string) => {
-    toggleDialogOpen();
     setSelectedSSID(value);
   };
 
@@ -42,28 +37,34 @@ const WifiScreen = () => {
     <ScreenLayout>
       <ResetReceiver />
       <View className="flex basis-3/5 bg-white rounded-t-3xl">
-        <Text className="mt-6 mb-4 text-center font-pbold text-black">
-          리시버와 연결할 WIFI를 선택하세요
-        </Text>
-        <FlatList
-          data={wifiList}
-          contentContainerClassName="flex gap-y-1 p-4 "
-          renderItem={({item: {SSID, BSSID}}) => (
-            <ListItem
-              title={SSID}
-              subTitle={BSSID}
-              value={SSID}
-              onPress={handleSSIDPress}
+        {selectedSSID ? (
+          <View className="flex py-6">
+            <WifiInfoInput
+              ssid={selectedSSID}
+              onCancel={() => setSelectedSSID(undefined)}
+              onSubmit={handleSubmitForm}
             />
-          )}
-        />
+          </View>
+        ) : (
+          <>
+            <Text className="mt-6 mb-4 text-center font-pbold text-black">
+              리시버와 연결할 WIFI를 선택하세요
+            </Text>
+            <FlatList
+              data={wifiList}
+              contentContainerClassName="flex gap-y-1 p-4 "
+              renderItem={({item: {SSID, BSSID}}) => (
+                <ListItem
+                  title={SSID}
+                  subTitle={BSSID}
+                  value={SSID}
+                  onPress={handleSSIDPress}
+                />
+              )}
+            />
+          </>
+        )}
       </View>
-      <WifiInfoDialog
-        visible={isDialogOpen}
-        ssid={selectedSSID}
-        closeDialog={toggleDialogOpen}
-        onClose={handleSubmitForm}
-      />
     </ScreenLayout>
   );
 };
